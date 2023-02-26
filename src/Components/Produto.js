f3db19af995458fcb836c8bfae8ad3b542961f2c
Dimitri Freitas <freitasdimitri7@gmail.com>
2023-02-25 23:51:32 -0300

    pages

--- src/Components/Produto.js
+++ src/Components/Produto.js
@@ -1,7 +1,33 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import styles from "./Produto.module.css";
 
 export const Produto = () => {
   const { id } = useParams();
-  return <div>{id}</div>;
+  const [itemData, setItemData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(`https://ranekapi.origamid.dev/json/api/produto/${id}`)
+      .then((r) => r.json())
+      .then((e) => setItemData(e));
+  }, [id]);
+  if (itemData) {
+    return (
+      <div className={styles.container}>
+        <img src={itemData.fotos[0].src} alt="" />
+        <div>
+          <h2>{itemData.nome}</h2>
+          <p className={styles.preco}>{"R$ " + itemData.preco}</p>
+          <p className={styles.desc}>{itemData.descricao}</p>
+          <button>Comprar</button>
+        </div>
+        <div>
+          <p className={styles.descR}>{itemData.descricao}</p>
+        </div>
+        <img src={itemData.fotos[1].src} alt="" />
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
